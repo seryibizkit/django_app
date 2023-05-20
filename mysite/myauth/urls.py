f31d7ddd0abf300c83b335fb91fe9451e3acf61f
Sergei Bystrov <seryibizkit@gmail.com>
2023-05-20 15:46:35 +0300

    M_08_Auth is done

---
+++ mysite/myauth/urls.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
+from .views import get_cookie_view, set_cookie_view, \
+    set_session_view, get_session_view, MyLogoutView
+
+
+app_name = "myauth"
+
+urlpatterns = [
+    # path("login/", login_view, name="login"),
+    path("login/",
+         LoginView.as_view(
+             redirect_authenticated_user=True,
+             template_name="myauth/login.html"),
+         name="login"),
+    # path("logout/", logout_view, name="logout"),
+    path("logout/", MyLogoutView.as_view(), name="logout"),
+    path("cookie/get/", get_cookie_view, name="cookie-get"),
+    path("cookie/set/", set_cookie_view, name="cookie-set"),
+    path("session/get/", get_session_view, name="session-get"),
+    path("session/set/", set_session_view, name="session-set"),
+]
